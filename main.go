6076685e85c4191f57fe55546d5184fbcd435a7c
Magnus Åsrud <magnusasrud92@gmail.com>
2024-07-31 00:44:48 +0200

    Style frontpage with light and dark modes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -20,13 +21,15 @@ func GetTemplates() *template.Template {
 
 func main() {
 	router := http.NewServeMux()
-
 	templates := GetTemplates()
+	fs := http.FileServer(http.Dir("./assets"))
 
 	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		templates.ExecuteTemplate(w, "index.html", nil)
 	})
 
-	fmt.Println("Running server on http://localhost:8080")
+	router.Handle("GET /assets/", http.StripPrefix("/assets/", fs))
+
+	log.Println("Running server on http://localhost:8080")
 	http.ListenAndServe(":8080", router)
 }
