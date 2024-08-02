3b7cd167bec29353c2b82b9bffceab3576f1e5d2
Magnus Åsrud <magnusasrud92@gmail.com>
2024-08-02 15:17:33 +0200

    robots.txt + meta description
    Try to please Google Lighthouse

--- main.go
+++ main.go
@@ -23,13 +23,20 @@ func main() {
 	router := http.NewServeMux()
 	templates := GetTemplates()
 	fs := http.FileServer(http.Dir("./assets"))
+	port := "8080"
 
+	// Assets such as images and stylesheets.
+	router.Handle("GET /assets/", http.StripPrefix("/assets/", fs))
+
+	router.HandleFunc("GET /robots.txt", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./public/robots.txt")
+	})
+
+	// Homepage and catch-all.
 	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		templates.ExecuteTemplate(w, "index.html", nil)
 	})
 
-	router.Handle("GET /assets/", http.StripPrefix("/assets/", fs))
-
-	log.Println("Running server on http://localhost:8080")
-	http.ListenAndServe(":8080", router)
+	log.Printf("Running server on http://localhost:%s", port)
+	http.ListenAndServe(fmt.Sprintf(":%s", port), router)
 }
